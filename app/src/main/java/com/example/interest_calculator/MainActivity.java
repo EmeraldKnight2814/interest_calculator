8cece7ec726b99ea28fe12db0fc72fc8b4a492d0
Daniel J Crowley <dcrow2814@gmail.com>
2023-02-02 14:58:48 -0600

    Cleaned up code, added optional features

--- app/src/main/java/com/example/interest_calculator/MainActivity.java
+++ app/src/main/java/com/example/interest_calculator/MainActivity.java
@@ -23,25 +23,63 @@ public class MainActivity extends AppCompatActivity {
         TextView compoundsET =(TextView)findViewById(R.id.compoundsET);
         TextView result = (TextView)findViewById(R.id.result);
 
-        int base  = Integer.parseInt(baseET.getText().toString());
+        double base  = Double.parseDouble(baseET.getText().toString());
+        System.out.println("Base: " + base);
         int compound  = Integer.parseInt(compoundsET.getText().toString());
-        int interest  = Integer.parseInt(interestET.getText().toString());
+        System.out.println("Compound: " + compound);
+        double interest  = Double.parseDouble(interestET.getText().toString());
+        System.out.println("Interest: " + interest);
         double fractional_interest = 0;
         double total = (double)base;
         double modifier = 0;
 
-        for(int i = 0; i < compound; i++){
-            // calculate fractional interest
-            fractional_interest = (interest / compound) / 10;
-
-            // calculate modifier
-            modifier = total * fractional_interest;
-
-            // calculate total
-            total += modifier;
+        if(compound == 0){
+            result.setText("Cannot divide by 0. Please enter another number for Compound.");
         }
+        else{
+            if(base < 0){
+                result.setText("Cannot compute with a negative base. Please enter another number for Base");
+            }
+            else{
+                if(interest < 0){
+                    result.setText("Cannot compute with negative interest. Please enter another number for Interest");
+                }
+                else{
+                    for(int i = 0; i < compound; i++){
+                        // calculate fractional interest
+                        fractional_interest = (interest / compound) / 100;
 
-        String string = "Amount at one year is $" + df.format(total);
-        result.setText(string);
+                        // calculate modifier
+                        modifier = total * fractional_interest;
+
+                        // calculate total
+                        total += modifier;
+                    }
+
+                    String string = "Amount at one year is $" + df.format(total);
+                    result.setText(string);
+                }
+            }
+        }
+    }
+
+    public void compound1(View trigger){
+        TextView compoundsET =(TextView)findViewById(R.id.compoundsET);
+        compoundsET.setText("1");
+    }
+
+    public void compound4(View trigger){
+        TextView compoundsET =(TextView)findViewById(R.id.compoundsET);
+        compoundsET.setText("4");
+    }
+
+    public void compound12(View trigger){
+        TextView compoundsET =(TextView)findViewById(R.id.compoundsET);
+        compoundsET.setText("12");
+    }
+
+    public void compound365(View trigger){
+        TextView compoundsET =(TextView)findViewById(R.id.compoundsET);
+        compoundsET.setText("365");
     }
 }
